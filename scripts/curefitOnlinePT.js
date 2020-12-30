af5aae20a5e1558721af94d9e80d3ae50bd9d644
pushpal chakrabarty <pushpalchakrabarty99@gmail.com>
2020-12-30 18:15:52 +0530

    landing page changes and profile changes

--- scripts/curefitOnlinePT.js
+++ scripts/curefitOnlinePT.js
@@ -12,17 +12,17 @@ var x = setInterval(function() {
   }
 }, 1000);
 
-let acc = document.getElementsByClassName("question");
-let i;
+// let acc = document.getElementsByClassName("question");
+// let i;
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    let answer = this.nextElementSibling;
-    if (answer.style.display === "block") {
-      answer.style.display = "none";
-    } else {
-      answer.style.display = "block";
-    }
-  });
-}  
+// for (i = 0; i < acc.length; i++) {
+//   acc[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     let answer = this.nextElementSibling;
+//     if (answer.style.display === "block") {
+//       answer.style.display = "none";
+//     } else {
+//       answer.style.display = "block";
+//     }
+//   });
+// }  
