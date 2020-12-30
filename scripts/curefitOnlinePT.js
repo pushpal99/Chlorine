3d5031e1928fca7a0f590d7b3f6c786ebebdca18
Harsh Gaur <harshgaur997@gmail.com>
2020-12-30 19:01:09 +0530

    cultfit js

--- scripts/curefitOnlinePT.js
+++ scripts/curefitOnlinePT.js
@@ -11,18 +11,3 @@ var x = setInterval(function() {
     document.getElementById("limitedoffer").innerHTML = "EXPIRED";
   }
 }, 1000);
-
-let acc = document.getElementsByClassName("question");
-let i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    let answer = this.nextElementSibling;
-    if (answer.style.display === "block") {
-      answer.style.display = "none";
-    } else {
-      answer.style.display = "block";
-    }
-  });
-}  
