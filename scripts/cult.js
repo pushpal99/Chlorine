3f5a0c06000dda657670737cb3aefdd4b78560ab
ankur <ankurp31031@gmail.com>
2020-12-31 13:05:32 +0530

    e

--- scripts/cult.js
+++ scripts/cult.js
@@ -12,9 +12,7 @@ function dropdown(){
 function myFunction() {
     document.getElementById("dropdown-contentmedical").classList.toggle("show");
   }
-  function myFunction() {
-    document.getElementById("dropdown-contentaccount").classList.toggle("view");
-  }
+
 
 // var acc = document.getElementsByClassName("accordion");
 // var i;
